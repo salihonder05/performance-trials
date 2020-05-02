b92f5b7f552da6f00a58e59b5e8710abdbecc888
Salih O <salih@twelve12.com>
2020-05-02 04:20:22 +0300

    Some trials

--- index.php
+++ index.php
@@ -1,22 +1,94 @@
-<?php 
-
-
-
-
-
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="tr">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 </head>
-<body>
-	asdasd
+<body id="body">
+	
+	
+
+
+
+
+	<script>
+
+		var url = "service.php";
+		var questorUrl = "service.php?getQueryID=True";
+
+
+
+
+
+		if(window.localStorage.getItem('localQueryID') !== null) {
+			// Local Query ID Var ise...
+				var localQueryID = window.localStorage.getItem('localQueryID');
+
+				$.getJSON(questorUrl, function(data) {
+					var queryID = data.key;
+					//document.write(data[0].key);
+				if (queryID === localQueryID) {
+					// Sorgu sonucunda son query id aynı bulundu, değişiklik talep edilmedi.
+						var data = window.localStorage.getItem('siteData');
+						data = JSON.parse(data);
+						const copy = []; 
+						data.forEach(function(item){ 
+							copy.push(item.id + '=' + item.number + '<br>'); 
+						}); 
+						document.write(copy); 
+				}
+				else {
+					// Sorgu sonucunda son query farklı bulundu, değişiklik istendi.
+						$.getJSON(url, function(data) {
+							data = JSON.stringify(data);
+							var queryID = data[0].key;
+							//İlk defa ziyaret edildi. Query ID kaydedildi.
+							var localQueryID = window.localStorage.setItem('localQueryID', queryID);
+							var localQueryID = window.localStorage.setItem('siteData', data);
+
+							var data = window.localStorage.getItem('siteData');
+							data = JSON.parse(data);
+							const copy = []; 
+							data.forEach(function(item){ 
+								copy.push(item.number + '<br>'); 
+							}); 
+							document.write(copy); 
+					})
+				}
+				})
+
+
+		}
+		else {
+			// Local Query ID Yok ise...
+				$.getJSON(questorUrl, function(data) {
+					var queryID = data.key;
+					//İlk defa ziyaret edildi. Query ID kaydedildi.
+					var localQueryID = window.localStorage.setItem('localQueryID',queryID);
+				})
+				$.getJSON(url, function(data) {
+					//İlk defa ziyaret edildi. Datalar kaydedildi.
+					data = JSON.stringify(data);
+					var localQueryID = window.localStorage.setItem('siteData',data);
+				})
+				var data = window.localStorage.getItem('siteData');
+				data = JSON.parse(data);
+				const copy = []; 
+				data.forEach(function(item){ 
+					copy.push(item.number + '<br>'); 
+				}); 
+				document.write(copy); 
+
+		}
+
+		
+		
+
+
+	</script>
+
 </body>
 </html>
---
+++ service.php
@@ -0,0 +1,45 @@
+<?php
+include('db.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header("Content-Type: application/json; charset=UTF-8");
+
+$dbhost = DBHOST;
+$dbuser = DBUSER;
+$dbname = DBNAME;
+$dbpass = DBPASS;
+
+$dbh = new PDO(sprintf('mysql:host=%s;dbname=%s', $dbhost, $dbname), $dbuser, $dbpass);
+
+
+
+$sql = "SELECT count(id) FROM `table1` ";
+$result = $dbh->prepare($sql);
+$result->execute();
+$number_of_rows = $result->fetchColumn();
+
+$statement = $dbh->prepare("SELECT * FROM table1");
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$key = sha1(serialize($results));
+
+if($_GET) {
+	if ($_GET["getQueryID"] == true) {
+		$QueryResult = [];
+		$QueryResult = ["key" => $key,"status" => true];
+		$QueryResult = json_encode($QueryResult);
+		print $QueryResult ;
+	}
+}
+else {
+	$results += ["key" => $key,"status" => true];
+	$json = json_encode($results);
+	print $json ;
+}
+
+
+
+$dbh = null;
+?>
